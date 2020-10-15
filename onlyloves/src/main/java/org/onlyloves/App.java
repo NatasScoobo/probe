456d2f434645d965d4507c6fea9fa6b887163c43
Sergey Ulasevich <sergey_only@mail.ru>
2020-10-15 20:45:36 +0300

    new files

---
+++ onlyloves/src/main/java/org/onlyloves/App.java
@@ -0,0 +1,13 @@
+package org.onlyloves;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
